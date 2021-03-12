dbc9ee8c651039cedf50e7471a680643dec130fd
Bennett Collier <bennettcollier@Bennetts-MacBook-Pro.local>
2021-03-11 19:38:29 -0800

    removed reactstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,36 @@
+import { GlobalProvider } from './utils/GlobalState';
 import logo from './logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import Landing from './pages/Landing';
+import Home from './pages/Home';
+import PlanEvent from './pages/PlanEvent';
+import Event from './pages/Event';
+import Recipes from './pages/Recipes';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Nav from './components/Nav';
 
-import { Container, Row, Col, Button, Alert, Breadcrumb, Card, Form } from 'react-bootstrap'
 
 
 function App() {
   return (
-    <div className="App">
-      <img src={logo} style={{ maxWidth: '10%', margin: '0.1rem' }} alt="logo" />
-      <h1 style={{ display: 'inline', fontSize: '3.5rem', color: 'navy' }}>Say Hello To Your POTLUCK PLANNER</h1>
-      <h2 style={{ display: 'inline', fontSize: '2.5rem', color: 'navy' }}>Let's Start Cooking</h2>
-      <Container>
-      <Form>
-        <Form.Group>
-          <Form.Label>Name</Form.Label>
-          <Form.Control placeholder="First and Last Name" />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label>Email</Form.Label>
-          <Form.Control type="email" placeholder="Example@email.com" />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Button variant="primary" type="submit">Register Now!</Button>
-      </Form>
-      </Container>
-      <h3 style={{ display: 'inline', fontSize: '2.5rem', color: 'navy' }}>Already a member?</h3>
-      <h3 style={{ display: 'inline', fontSize: '2.5rem', color: 'navy', fontWeight: 'bold' }}>LOG IN HERE</h3>
-    </div>
+    <Router>
+      <div className="App">
+        <img src={logo} style={{ maxWidth: '10%', margin: '0.1rem' }} alt="logo" />
+        <GlobalProvider>
+          <Nav />
+          <Switch>
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/home" component={Home} />
+            <Route exact path="/potluck" component={PlanEvent} />
+            <Route exact path="/event/:eventId" component={Event} />
+            <Route exact path="/recipes" component={Recipes} />
+            <Route exact path="/signup" component={Signup} />
+            <Route exact path="/login" component={Login} />
+          </Switch>
+        </GlobalProvider>
+      </div>
+    </Router>
   );
 }
 
