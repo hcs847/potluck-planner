d71b640017d027394752ff565bf3b39522200745
Hila Caspi <caspi.home@gmail.com>
2021-03-14 16:23:03 -0700

    adjust queries

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -3,23 +3,15 @@ import gql from 'graphql-tag';
 export const QUERY_EVENTS = gql`
 query events {
     events{
-     _id
+        _id
     eventName
-    message
-    date
-    time
+      message
+      date
+      time
     location
-    host {
-      firstName
-      lastName
-      email
-    }
-    guests {
-      firstName
-    }
-    dishes {
-      dishName
-    }
+   host
+    guests
+    dishes 
   }
   }
   
