bb25bf10a989f0fcb058328656f9624c43493927
Stephen Beiter <stephenbeiter@gmail.com>
2021-03-21 03:06:34 -0700

    added pwa

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 
 
@@ -11,3 +12,5 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+serviceWorkerRegistration.register();
